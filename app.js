513cfea519d21d2205756caeaa58e1defb3cdf61
M-ilyas1 <mohammadilyas6800@gmail.com>
2022-10-29 05:08:14 -0700

    add

--- app.js
+++ app.js
@@ -114,17 +114,54 @@
 // 3. ==========================================================================================================
 
 
+// let input = document.getElementById("myInput")
+
+// let todoList = document.querySelector("#list")
+
+// let list = []
+
+// let todoApp = {
+//     addFun(){
+//         if(input.value){
+//             list.push(input.value)
+//             input.value = ""
+//         }
+//         this.myFun()
+//     },
+    // myFun(){
+    //     todoList.innerHTML = ""
+    //     list.forEach((value, index) =>{
+    //         todoList.innerHTML += `<p class="myList">${value}
+    //         <button id="x-btn" onclick="todoApp.delete(${index})">
+    //         <i class='bx bxs-message-square-x'></i></button></p>`
+    //     })
+//     },
+//     delete(index){
+//         list.splice(index, 1)
+//         this.myFun()
+//     },
+//     clear(){
+//         list.splice(0, list.length)
+//         this.myFun()
+
+//     }
+// }
+
+
+// 4. ============================================================================================================
+
+
 let input = document.getElementById("myInput")
 
 let todoList = document.querySelector("#list")
 
-let list = []
+let list =[]
 
 let todoApp = {
     addFun(){
         if(input.value){
-            list.push(input.value)
-            input.value = ""
+            list.push(input.value);
+            input.value =  ""
         }
         this.myFun()
     },
@@ -143,6 +180,7 @@ let todoApp = {
     clear(){
         list.splice(0, list.length)
         this.myFun()
-
+        
     }
 }
+
