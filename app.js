fe63ca07e69a19f07e08b2d20f05f26b6e0108a6
M-ilyas1 <mohammadilyas6800@gmail.com>
2022-10-28 23:19:51 -0700

    add

--- app.js
+++ app.js
@@ -1,27 +1,65 @@
-let input = document.getElementById("myInput")
-console.log(input)
+// let input = document.getElementById("myInput")
+// // console.log(input)
 
-let todoList = document.querySelector("#list")
-console.log(todoList);
+// let todoList = document.querySelector("#list")
+// // console.log(todoList);
 
-let list = [];
+// let list = [];
+
+
+// let todoApp = {
+//     addFun(){
+//         if (input.value) {
+//             list.push(input.value)
+//             input.value = ""
+            
+//         }
+//         this.myFun()
+//     },
+//     myFun(){
+//         todoList.innerHTML = ""
+//         list.forEach((value, index) =>{
+//             todoList.innerHTML += `<p class="myList">${value}
+//             <button id="x-btn" onclick="todoApp.delete(${index})">
+//             <i class='bx bxs-message-square-x'></i></button></p>` 
+//         })
+//     },
+//     delete(index){
+//         list.splice(index, 1)
+//         this.myFun()
+//     },
+//     clear(){
+//         list.splice(0, list.length)
+//         this.myFun()
+//     }
+// }
+
+
+// 1. ======================================================================================================
+
+let input = document.getElementById("myInput");
+// console.log(input);
+let todolsit = document.querySelector("#list");
+// console.log(todolsit);
+
+let list = []
 
 
 let todoApp = {
     addFun(){
-        if (input.value) {
+        if(input.value){
             list.push(input.value)
             input.value = ""
-            
         }
         this.myFun()
     },
     myFun(){
-        todoList.innerHTML = ""
-        list.forEach((value, index) =>{
-            todoList.innerHTML += `<p class="myList">${value}
+        todolsit.innerHTML = ""
+        list.forEach((value, index)=>{
+            todolsit.innerHTML += `<p class="myList">${value}
             <button id="x-btn" onclick="todoApp.delete(${index})">
-            <i class='bx bxs-message-square-x'></i></button></p>` 
+            <i class='bx bxs-message-square-x'></i></button</p>`
+
         })
     },
     delete(index){
