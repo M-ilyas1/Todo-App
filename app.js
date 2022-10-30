b059833af1b9b42fe3ffca555bc752e497ca11ba
M-ilyas1 <mohammadilyas6800@gmail.com>
2022-10-30 02:44:51 -0700

    add

--- app.js
+++ app.js
@@ -155,42 +155,78 @@
 // 8. ============================================================================================================
 
 
-let input = document.getElementById("myInput")
+// let input = document.getElementById("myInput")
 
-let todoList = document.querySelector("#list")
+// let todoList = document.querySelector("#list")
 
-let list = []
+// let list = []
 
-let todoApp = {
-    addFun(){
-        if(input.value){
-            list.push(input.value)
-            input.value = ""
-        }
-        this.myFun()
-    },
-    myFun(){
-        todoList.innerHTML = ""
-        list.forEach((value, index) =>{
-            todoList.innerHTML += `<p class="myList">${value}
-            <button id="x-btn" onclick="todoApp.delete(${index})">
-            <i class='bx bxs-message-square-x'></i></button></p>`
-        })
-    },
-    delete(index){
-        list.splice(index, 1)
-        this.myFun()
-    },
-    clear(){
-        list.splice(0, list.length)
-        this.myFun()
+// let todoApp = {
+//     addFun(){
+//         if(input.value){
+//             list.push(input.value)
+//             input.value = ""
+//         }
+//         this.myFun()
+//     },
+//     myFun(){
+//         todoList.innerHTML = ""
+//         list.forEach((value, index) =>{
+//             todoList.innerHTML += `<p class="myList">${value}
+//             <button id="x-btn" onclick="todoApp.delete(${index})">
+//             <i class='bx bxs-message-square-x'></i></button></p>`
+//         })
+//     },
+//     delete(index){
+//         list.splice(index, 1)
+//         this.myFun()
+//     },
+//     clear(){
+//         list.splice(0, list.length)
+//         this.myFun()
 
-    }
-}
+//     }
+// }
 
 
 // 9. ===========================================================================================================
 
+
+// let input = document.getElementById("myInput")
+
+// let todoList = document.querySelector("#list")
+
+// let list = []
+
+// let todoApp = {
+//     addFun(){
+//         if(input.value){
+//             list.push(input.value)
+//             input.value = ""
+//         }
+//         this.myFun()
+//     },
+//     myFun(){
+//         todoList.innerHTML = ""
+//         list.forEach((value, index) =>{
+//             todoList.innerHTML += `<p class="myList">${value}
+//             <button id="x-btn" onclick="todoApp.delete(${index})">
+//             <i class='bx bxs-message-square-x'></i></button></p>`
+//         })
+//     },
+//     delete(index){
+//         list.splice(index, 1)
+//         this.myFun()
+//     },
+//     clear(){
+//         list.splice(0, list.length)
+//         this.myFun()
+
+//     }
+// }
+
+
+// 10. ===========================================================================================================
 
 let input = document.getElementById("myInput")
 
