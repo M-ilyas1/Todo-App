186769dde053b7ca982a8d20d32de0905d8ab9d5
M-ilyas1 <mohammadilyas6800@gmail.com>
2022-11-05 08:59:34 -0700

    add

--- app.js
+++ app.js
@@ -563,6 +563,46 @@
 // 16. ============================================================================================================
 
 
+// let input =document.getElementById("myInput")
+
+// let todoList = document.querySelector("#list")
+
+
+// let list = []
+
+// let todoApp = {
+//     addFun(){
+//         if(input.value){
+//             list.push(input.value)
+//             input.value = ""
+//         }
+//         this.myFun()
+//     },
+//     myFun(){
+//         todoList.innerHTML = ""
+//         list.forEach((value , index) => {
+//             todoList.innerHTML += `<p class="myList">${value}
+//             <button id="x-btn" onclick="todoApp.delete(${index})">
+//             <i class='bx bxs-message-square-x'></i></button></p>`
+
+//         })
+//     },
+//     delete(index){
+//         list.splice(index, 1)
+//         this.myFun()
+//     },
+//     clear(){
+//         list.splice(0, list.length)
+//         this.myFun()
+        
+//     }
+
+// }
+
+
+// 17. ============================================================================================================
+
+
 let input =document.getElementById("myInput")
 
 let todoList = document.querySelector("#list")
@@ -598,6 +638,3 @@ let todoApp = {
     }
 
 }
-
-
-// 17. ============================================================================================================
